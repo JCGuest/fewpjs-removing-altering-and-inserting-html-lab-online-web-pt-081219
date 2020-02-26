6cfb959594e5a89f9e7e2a3e33e7c4355750909b
JCGuest <jcguest90@gmail.com>
2020-02-25 22:58:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+// <!DOCTYPE html>
+// <html>
+//   <head>
+//     <title>Creating and Inserting DOM Elements</title>
+//   </head>
+//   <script type="application/javascript">
+//   </script>
+//   <body>
+//     <main id="main"></main>
+//     <script type="text/javascript" src="index.js"></script>
+//   </body>
+// </html>
+
+let main = document.querySelector("main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Your-Name is the champion";
